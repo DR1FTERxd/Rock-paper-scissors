9185676a695add2e2b67697bb2728d74947f46a7
DR1FT3R <Eligijuskont@gmail.com>
2022-03-13 13:51:08 +0200

    add

--- game.js
+++ game.js
@@ -1,74 +1,70 @@
-function computerPlay() {
-    var items = ["Rock","Paper","Scissors"];
-    var item = items[Math.floor(Math.random()*items.length)];
-    return item;
-};
+let playerScore = 0;
+let computerScore = 0;
+game();
 
 
 
 function playRound(playerSelection, computerSelection){
     if (playerSelection === "Rock" && computerSelection === "Rock"){
-        return ("Tie")
+        console.log("Tie")
     } else if (playerSelection === "Rock" && computerSelection === "Paper"){
-        return ("Computer wins")
+        computerScore++;
+        console.log("Computer wins")
     } else if (playerSelection === "Rock" && computerSelection === "Scissors"){
-        return ("Human wins")
+        playerScore++;
+        console.log("Human wins")
     };
 
 
     if (playerSelection === "Paper" && computerSelection === "Paper"){
-        return ("Tie")
+        console.log("Tie")
     } else if (playerSelection === "Paper" && computerSelection === "Scissors"){
-        return ("Computer wins")
+        computerScore++;
+        console.log("Computer wins")
     } else if (playerSelection === "Paper" && computerSelection === "Rock"){
-        return ("Human wins")
+        playerScore++;
+        console.log("Human wins")
     };
 
     if (playerSelection === "Scissors" && computerSelection === "Scissors"){
-        return ("Tie")
+        console.log("Tie")
     } else if (playerSelection === "Scissors" && computerSelection === "Rock"){
-        return ("Computer wins")
+        computerScore++;
+        console.log("Computer wins")
     } else if (playerSelection === "Scissors" && computerSelection === "Paper"){
-        return ("Human wins")
-    };
+        playerScore++;
+        console.log("Human wins")
+    }
+    console.log("Computer score" + computerScore );
+    console.log("Player score" + playerScore)
+}
 
+function computerPlay() {
+    let max = 4;
+    let min = 1;
+    var items = ["Rock","Paper","Scissors"];
+    var item = items[Math.floor(Math.random()*items.length)];
+    return item;
+}
 
-};
-
-function game(playRound){
+function game(playRound) {
     for (let i = 0; i < 5; i++) {
-        function playRound(playerSelection, computerSelection){
-            if (playerSelection === "Rock" && computerSelection === "Rock"){
-                return ("Tie")
-            } else if (playerSelection === "Rock" && computerSelection === "Paper"){
-                return ("Computer wins")
-            } else if (playerSelection === "Rock" && computerSelection === "Scissors"){
-                return ("Human wins")
-            };
-        
-        
-            if (playerSelection === "Paper" && computerSelection === "Paper"){
-                return ("Tie")
-            } else if (playerSelection === "Paper" && computerSelection === "Scissors"){
-                return ("Computer wins")
-            } else if (playerSelection === "Paper" && computerSelection === "Rock"){
-                return ("Human wins")
-            };
-        
-            if (playerSelection === "Scissors" && computerSelection === "Scissors"){
-                return ("Tie")
-            } else if (playerSelection === "Scissors" && computerSelection === "Rock"){
-                return ("Computer wins")
-            } else if (playerSelection === "Scissors" && computerSelection === "Paper"){
-                return ("Human wins")
-            };
-        
-        
-        };
-     };
-};
+        console.log("Round #" + i)
+        const playerSelection = "Rock";
+        const computerSelection = computerPlay();
+        playRound(playerSelection, computerSelection);
+    }
+    console.log("Game over")
+    if(playerScore > computerScore) {
+        console.log("You are the Winner")
+    } else if (playerScore == computerScore) {
+        console.log("Its a tie")
+    } else if (playerScore < computerScore) {
+        console.log ("Computer wins")
+    }
+}
 
 
-const playerSelection = "Rock";
+const playerSelection = prompt();
 const computerSelection = computerPlay();
 console.log(playRound(playerSelection, computerSelection));
